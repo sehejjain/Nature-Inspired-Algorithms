4934681f23589a4a211b70559b24af542fe656c3
Sehej Jain <sehejjain@outlook.com>
2021-05-28 12:33:50 +0530

    Added Results

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-from algorithms import ParticleSwarmOptimization, WhaleOptimizationAlgorithm, CuckooSearch, GreyWolfOptimizer, FruitFly, SquirrelSearchAlgorithm, KrillHerd
-from benchmarks import Ackley, Sphere
-import logging
+from algorithms import ParticleSwarmOptimization, WhaleOptimizationAlgorithm, CuckooSearch, GreyWolfOptimizer, FruitFly, SquirrelSearchAlgorithm, KrillHerd, WhaleOptimizationAlgorithm, WaterWaveOptimization
+from benchmarks import Ackley, Sphere, Schwefel, Schwefel2, XinSheYangN2, Rosenbrock, Griewank, Levy13, Rastrigin, Bukin6, Easom, Camel3, Shubert, Eggholder, Crossit, Michalewicz, Holdertable
+import logging, os
 import pandas as pd
-
-logging.basicConfig()
-logger = logging.getLogger('cs')
-logger.setLevel('INFO')
-# cs = GreyWolfOptimizer(func=Ackley(), iterations=500, debug=True)
-# best_sol, best_val = cs.run()
-# logger.info("best sol:{sol}, best val:{val}".format(sol=best_sol, val=best_val))
-
-list1 = []
 import tqdm
-filename = "ackley_gwo.csv"
-try:
-    orig_df = pd.read_csv(filename)
-    orig_df = orig_df.drop(columns=["Unnamed: 0"])
-except:
-    pass
 
-for i in tqdm.trange(10):
-    cs = GreyWolfOptimizer(func=Ackley(), iterations=500, debug=False)
-    best_sol, best_val = cs.run()
-    list1.append(best_val)
-    # best_sol, best_val = cs.run()
-    # logger.info("best sol:{sol}, best val:{val}".format(sol=best_sol, val=best_val))
+for benchmark in [Eggholder, Crossit, Michalewicz, Holdertable]:
+    # benchmark = Shubert
+    for algo in [ ParticleSwarmOptimization, CuckooSearch, GreyWolfOptimizer, FruitFly, KrillHerd, WhaleOptimizationAlgorithm, WaterWaveOptimization]:
 
-import pandas as pd
+        filename = "Results/"+benchmark.__name__+"/"+algo.__name__+".csv"
+        list1 = []
 
-df = pd.DataFrame(list1, columns=["fitness"])
-try:
-    df = df.append(orig_df) 
-except :
-    pass
 
-df.to_csv(filename)
+        try:
+            orig_df = pd.read_csv(filename)
+            orig_df = orig_df.drop(columns=["Unnamed: 0"])
+        except:
+            pass
+
+        for i in tqdm.trange(10):
+            cs = algo(func=benchmark(), iterations=300, debug=False)
+            best_sol, best_val = cs.run()
+            list1.append(best_val)
+
+        df = pd.DataFrame(list1, columns=["fitness"])
+        try:
+            df = df.append(orig_df) 
+        except :
+            pass
+
+        try:
+            df.to_csv(filename)
+        except:
+            os.makedirs("Results/"+benchmark.__name__)
+            df.to_csv(filename)
 
